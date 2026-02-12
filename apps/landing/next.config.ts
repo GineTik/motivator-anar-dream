381ad70fc94480abcfe4bb53bdecdef2ae5aed79
Slava <slava@tedgustaf.com>
2026-02-12 22:41:38 +0200

    Update cd & next.config

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
+import path from "path";
 
 const nextConfig: NextConfig = {
-    // Your Next.js config here
+    turbopack: {
+        root: path.join(__dirname, "../.."),
+    },
 };
 
 export default withPayload(nextConfig);
